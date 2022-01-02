4f20fc2524e23db99a3dd4d0285bc35fff99b8b1
Arif-Hasan-Zitu <96912959+Arif-Hasan-Zitu@users.noreply.github.com>
2022-01-02 04:45:59 -0800

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,37 @@
+(function($) {
+    'use strict';
+
+    $('.ui-ux').rProgressbar({
+        percentage: 75,
+        fillBackgroundColor: '#1abc9c'
+    });
+    $('.webdevelopment').rProgressbar({
+        percentage: 90,
+        fillBackgroundColor: '#2ecc71'
+    });
+
+
+
+    $('.marketing').rProgressbar({
+        percentage: 65,
+        fillBackgroundColor: '#9b59b6',
+        backgroundColor: '#e67e22'
+    });
+
+
+
+    $('.javascript').rProgressbar({
+        percentage: 65,
+        fillBackgroundColor: '#34495e'
+    });
+
+
+
+    $('.jquery').rProgressbar({
+        percentage: 95,
+        fillBackgroundColor: '#f1c40f',
+        backgroundColor: '#e67e22'
+    });
+
+
+})(jQuery);
